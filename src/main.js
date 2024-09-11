0023f786df7c7e3c0532fb4d973ac8388b9b6a23
Itamar Medeiros <itamarjunior@outlook.com.br>
2024-09-11 18:10:13 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function dizOi(nome) {
+    return `Olá ${nome}`;
+}
+const resultadoMultiplicacao = multiplicar(5, 3);
+const mensagemSaudacao = dizOi("Maria");
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
